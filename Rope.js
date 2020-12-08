7dac1e1cf217dc9c3d6398527dba5bc167f618a2
AnannyaLancelot <72078305+AnannyaLancelot@users.noreply.github.com>
2020-12-08 19:30:31 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,13 @@
+class Rope{
+    constructor(bodyA, bodyB){ 
+        var options = { 
+bodyA: bodyA, 
+bodyB: bodyB 
+ }
+this.rope=Constraint.create(options);
+ World.add(world, this.rope); } 
+ display()
+ { var pointA = this.rope.bodyA.position; 
+   var pointB = this.rope.bodyB.position; 
+   strokeWeight(4); 
+   line(pointA.x, pointA.y, pointB.x, pointB.y); } }
